04671fd08c1d486b162c89bc2a61cfb12065d0da
QuadlcorE <62242772+QuadlcorE@users.noreply.github.com>
2022-09-07 21:01:09 +0100

    Fixed the nav-bar

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask,render_template
+from getFunction import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    top_games = get_top_games()
+    for game in top_games:
+        game["background"] = get_game_background(game["slug"])
+    
+    return render_template("index.html", top_games = top_games)
+
+@
